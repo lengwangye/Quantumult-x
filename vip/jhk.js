cf0b797dada01ac38389257fe5fb9623dcb92997
lengwangye <34705985+lengwangye@users.noreply.github.com>
2023-03-05 13:09:05 +0800

    Update jhk.js

--- vip/jhk.js
+++ vip/jhk.js
@@ -1,6 +1,18 @@
+/******************************
+
+脚本功能：传图加画框
+软件版本：v1.2.0
+脚本作者：起风了
+更新时间：2023-3-5
+问题反馈：QQ+980807262
+使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+
+*******************************
+
 [rewrite_local]
 
-^https:\/\/prod\.jiahk\.cn\/api\/v1\/findUserInfo url script-response-body 11.js
+^https:\/\/prod\.jiahk\.cn\/api\/v1\/findUserInfo url script-response-body https://raw.githubusercontent.com/lengwangye/Quantumult-x/main/vip/jhk.js
 
 [mitm] 
 
